b58df9dd1adca4410eaaccf8359ba32d36c6d2bc
JuanP <pastranajc@hotmail.com>
2016-06-04 09:48:26 -0500

    Add project files.

---
+++ BuildingBlock.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuildingBlock.Web.Startup))]
+namespace BuildingBlock.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
